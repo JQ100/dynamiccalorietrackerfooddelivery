4c12a0159a31f507fe3fd2b848bdb6529e9f2370
Jerry Q <36783706+JQ100@users.noreply.github.com>
2023-11-14 15:34:21 -0800

    Created blueprints and updated restaurant bp to work with basic welcome info

--- blueprints/restaurantmenu/restaurantmenu.py
+++ blueprints/restaurantmenu/restaurantmenu.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, redirect
+from blueprints.restaurantmenu.restaurant import Restaurant
 
 restaurantmenu_bp = Blueprint("restaurantmenu", __name__, template_folder="templates")
 
 @restaurantmenu_bp.route("/restaurantmenu")
 def restaurantmenu():
-    return "This is the Restaurant Menu page"
+    return "This is the Restaurant Menu page. Please choose from the available options."
+
+# menu coming soon
