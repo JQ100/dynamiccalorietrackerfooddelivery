08e992a8e8d3689016a8db8473701c4b22da3793
Jerry Qian <jerrybbtt@gmail.com>
2023-11-26 17:58:24 -0800

    implement insert and read operations for restaurant menu

--- dynamic_calorie_tracker/blueprints/restaurant_menu/restaurant_menu.py
+++ dynamic_calorie_tracker/blueprints/restaurant_menu/restaurant_menu.py
@@ -1,8 +1,29 @@
-from flask import Blueprint, render_template, redirect
-# from blueprints.restaurant_menu.restaurant import Restaurant
+from flask import Blueprint, render_template, request, redirect
+from ...models.restaurant_menu import RestaurantMenu
+from ...extensions import db
 
-restaurant_menu_bp = Blueprint("restaurant_menu", __name__, template_folder="templates")
+restaurant_menu_bp = Blueprint(
+    "restaurant_menu", 
+    __name__, 
+    template_folder="templates", 
+    static_folder='static',
+)
 
-@restaurant_menu_bp.route("/restaurant_menu")
+
+@restaurant_menu_bp.route("/restaurant_menu", methods=["GET", "POST"])
 def restaurant_menu():
-    return "This is the Restaurant Menu page. Please choose from the available options."
+    if request.method == 'POST':
+        name = request.form['name']
+        new_restaurant_menu = RestaurantMenu(name=name)
+
+        try:
+            db.session.add(new_restaurant_menu)
+            db.session.commit()
+            return redirect('/restaurant_menu')
+        except:
+            return 'There was an issue adding your menu'
+
+    else:
+        restaurant_menus = RestaurantMenu.query.order_by(
+            RestaurantMenu.date_created).all()
+        return render_template('menu_index.html', restaurant_menus=restaurant_menus)
