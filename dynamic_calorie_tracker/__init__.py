d7a8ac088d64535d88b530af15204135fa19cd26
Jerry Qian <jerrybbtt@gmail.com>
2023-12-03 20:12:51 -0800

    init db data and remove customer_order table

--- dynamic_calorie_tracker/__init__.py
+++ dynamic_calorie_tracker/__init__.py
@@ -3,7 +3,6 @@ from .extensions import db
 
 from .blueprints.main.main import main_bp
 from .blueprints.customer.customer import customer_bp
-from .blueprints.customer_order.customer_order import customer_order_bp
 from .blueprints.transaction.transaction import transaction_bp
 from .blueprints.recipes.recipes import recipes_bp
 from .blueprints.menu_item.menu_item import menu_item_bp
@@ -23,7 +22,6 @@ def create_app():
 
     app.register_blueprint(main_bp)
     app.register_blueprint(customer_bp)
-    app.register_blueprint(customer_order_bp)
     app.register_blueprint(transaction_bp)
     app.register_blueprint(recipes_bp)
     app.register_blueprint(menu_item_bp)
