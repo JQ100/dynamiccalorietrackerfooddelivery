1cdd98707711c707c2975922af709fb3bcfab425
ErikMelchers <melcherserik@gmail.com>
2023-12-04 16:25:43 -0500

    connecting to database
    :)

--- dynamic_calorie_tracker/blueprints/recipes/recipes_create.py
+++ dynamic_calorie_tracker/blueprints/recipes/recipes_create.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import create_engine, MetaData, Table, select
 
 
 class Recipes:
@@ -9,30 +9,38 @@ class Recipes:
         self.nutritional_data = nutritional_data
         self.link = link
     
-from sqlalchemy import create_engine, MetaData, Table
 
-# MySQL connection string format: 'mysql+mysqlconnector://username:password@host:port/database_name'
-# Replace 'username', 'password', 'host', 'port', and 'database_name' with your MySQL credentials
-connection_string = 'mysql+mysqlconnector://username:password@host:port/database_name'
+username = 'root'
+password = '1234'
+host = '127.0.0.1'
+database_name = 'recipelol'
 
-# Create an SQLAlchemy engine
-engine = create_engine(connection_string)
+# MySQL connection URL format: mysql+pymysql://username:password@host/database_name
+db_url = f'mysql+pymysql://{username}:{password}@{host}/{database_name}'
 
-# Create a metadata object
+# Create the engine
+engine = create_engine(db_url)
+
+# Test the connection by attempting to connect
+try:
+    with engine.connect() as connection:
+        print("Connected to MySQL database!")
+except Exception as e:
+    print("Failed to connect to the database:", e)
+
+
 metadata = MetaData()
 
-# Example: Reflect an existing database table
-# Replace 'your_table_name' with the name of the table you want to work with
-your_table = Table('your_table_name', metadata, autoload=True, autoload_with=engine)
+# Define the 'recipes' table using SQLAlchemy's Table object
+recipes = Table('recipes', metadata, autoload_with=engine)
 
-# Establish a connection
+
+# Selecting 'name' and 'calorie' columns from the 'recipes' table
+query = select(recipes.c.name, recipes.c.calorie, recipes.c.link)
+
 with engine.connect() as connection:
-    # Example: Execute a SELECT query
-    query = your_table.select()
     result = connection.execute(query)
+    rows = result.fetchall()
 
-    # Fetch and display data
-    for row in result:
-        print(row)
-
-    result.close()
+    for row in rows:
+        print(row[0], row[1], row[2])  # Accessing the first and second elements in the row for name and calorie respectively
