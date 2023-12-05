05bf6a034f74374ae25bfbd1289e8b68edd99d28
Jerry Qian <jerrybbtt@gmail.com>
2023-12-04 20:13:06 -0800

    fix flask issue by commenting recipe bp from __init__.py

--- dynamic_calorie_tracker/__init__.py
+++ dynamic_calorie_tracker/__init__.py
@@ -4,7 +4,7 @@ from .extensions import db
 from .blueprints.main.main import main_bp
 from .blueprints.food_delivery.food_delivery import food_delivery_bp
 from .blueprints.customer.customer import customer_bp
-from .blueprints.recipes.recipes import recipes_bp
+# from .blueprints.recipes.recipes import recipes_bp
 from .blueprints.menu_item.menu_item import menu_item_bp
 from .blueprints.restaurant.restaurant import restaurant_bp
 from .blueprints.order.order import order_bp
@@ -23,7 +23,7 @@ def create_app():
     app.register_blueprint(main_bp)
     app.register_blueprint(food_delivery_bp)
     app.register_blueprint(customer_bp)
-    app.register_blueprint(recipes_bp)
+    # app.register_blueprint(recipes_bp)
     app.register_blueprint(menu_item_bp)
     app.register_blueprint(restaurant_bp)
     app.register_blueprint(order_bp)
